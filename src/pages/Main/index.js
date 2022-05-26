11e586233528cdfc169f046c90821372b5529c34
saehee-park <hee00315@gmail.com>
2022-05-27 03:06:07 +0900

    modal창 생성 및 바로가기 버튼 url추가

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
+
 // @mui material components
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import Modal from "@mui/material/Modal";
+import Divider from "@mui/material/Divider";
+import Slide from "@mui/material/Slide";
+
+import CloseIcon from "@mui/icons-material/Close";
 
 // Material Kit 2 React components
 import MKBox from "components/MKBox";
@@ -17,6 +24,9 @@ import routes from "routes";
 import bgImage from "assets/images/bg-main.jpg";
 
 function Main() {
+  const [show, setShow] = useState(false);
+  const toggleModal = () => setShow(!show);
+
   return (
     <>
       <DefaultNavbar routes={routes} transparent light />
@@ -59,6 +69,7 @@ function Main() {
               variant="outlined"
               color="white"
               sx={{ color: ({ palette: { white } }) => white.main }}
+              href="Prediction"
             >
               바로가기
             </MKButton>
@@ -68,9 +79,45 @@ function Main() {
               variant="text"
               color="white"
               sx={{ color: ({ palette: { white } }) => white.main }}
+              onClick = {toggleModal}
             >
               자세히
             </MKButton>
+            <Modal open={show} onClose={toggleModal} sx={{ display: "grid", placeItems: "center" }}>
+              <Slide direction="down" in={show} timeout={500}>
+                <MKBox
+                  position="relative"
+                  width="500px"
+                  display="flex"
+                  flexDirection="column"
+                  borderRadius="xl"
+                  bgColor="white"
+                  shadow="xl"
+                >
+                  <MKBox display="flex" alginItems="center" justifyContent="space-between" p={2}>
+                    <MKTypography variant="h5">편한 버스란?</MKTypography>
+                    <CloseIcon fontSize="medium" sx={{ cursor: "pointer" }} onClick={toggleModal} />
+                  </MKBox>
+                  <Divider sx={{ my: 0 }} />
+                  <MKBox p={2}>
+                    <MKTypography variant="body2" color="secondary" fontWeight="regular">
+                      편한 버스란, 버스 혼잡도를 예측해주는 서비스입니다. 저희 서비스는 사용자에게 해당 시간에 버스 안이 얼마나 혼잡할 지를 예측하여 제공합니다.
+                      <br />
+                      <br />
+                      버스를 이용할 때 대부분의 사람들은 버스가 혼잡해도 공간이 전혀 없지 않는 이상 탐승에 크게 문제가 되지 않습니다.&apos;
+                      그러나 노인, 임산부, 영유아동반자 등 교통 약자는 혼잡한 버스를 이용하는 것이 어렵습니다.&apos;이에 저희는 버스 혼잡에 취약한 
+                      교통약자가 이동할 때, 출발 시간 선택에 도움을 주기 위해 해당 서비스를 기획하게 되었습니다.
+                    </MKTypography>
+                  </MKBox>
+                  <Divider sx={{ my: 0 }} />
+                  <MKBox display="flex" justifyContent="space-between" p={1.5}>
+                    <MKButton variant="gradient" color="dark" onClick={toggleModal}>
+                      close
+                    </MKButton>
+                  </MKBox>
+                </MKBox>
+              </Slide>
+            </Modal>
           </Grid>
         </Container>
       </MKBox>
